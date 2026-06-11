d877541dfc966912a3a03d942fa4dae3ec1224fe
Sensanaty <salevic@lu-ka.me>
2026-06-11 21:56:57 +0200

    Init repo

---
+++ BlueprintExtractor/Main.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using UnityModManagerNet;
+
+namespace BlueprintExtractor;
+
+public static class Main {
+  private static Harmony HarmonyInstance;
+  private static UnityModManager.ModEntry.ModLogger Log;
+
+  public static bool Load(UnityModManager.ModEntry modEntry) {
+    Log = modEntry.Logger;
+    modEntry.OnGUI = OnGUI;
+    HarmonyInstance = new Harmony(modEntry.Info.Id);
+
+    try {
+      HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+    }
+    catch {
+      HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+
+      throw;
+    }
+
+    return true;
+  }
+
+  private static void OnGUI(UnityModManager.ModEntry modEntry) { }
+
+  [HarmonyPatch(typeof(BlueprintsCache))]
+  public static class BlueprintsCaches_Patch {
+    private static bool Initialized;
+
+    [HarmonyPriority(Priority.First)]
+    [HarmonyPatch(nameof(BlueprintsCache.Init))]
+    [HarmonyPostfix]
+    public static void Init_Postfix() {
+      try {
+        if (Initialized) {
+          Log.Log("Already initialized blueprints cache.");
+
+          return;
+        }
+
+        Initialized = true;
+
+        Log.Log("Patching blueprints.");
+        // Insert your mod blueprint add/subtract/patch here
+        // Example
+        // MyAwesomeFeat.Patch();
+      }
+      catch (Exception e) {
+        Log.Log(string.Concat("Failed to initialize.", e));
+      }
+    }
+  }
+}
